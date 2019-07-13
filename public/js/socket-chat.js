14dc42300c7cc78308b40e4c161e2f2db95b728d
irving199627 <irving_rosado01@hotmail.com>
2019-07-13 00:11:16 -0500

    Chat finalizado v1

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -1,6 +1,7 @@
 var socket = io();
 
 var params = new URLSearchParams(window.location.search);
+
 if (!params.has('nombre') || !params.has('sala')) {
     window.location = 'index.html';
     throw new Error('El nombre y sala son necesarios');
@@ -11,23 +12,29 @@ var usuario = {
     sala: params.get('sala')
 };
 
+
+
 socket.on('connect', function() {
     console.log('Conectado al servidor');
 
     socket.emit('entrarChat', usuario, function(resp) {
         console.log('Usuarios conectados', resp);
+        renderizarUsuarios(resp);
     });
+
 });
 
 // escuchar
 socket.on('disconnect', function() {
+
     console.log('Perdimos conexión con el servidor');
+
 });
 
 
 // Enviar información
 // socket.emit('crearMensaje', {
-//     usuario: 'Fernando',
+//     nombre: 'Fernando',
 //     mensaje: 'Hola Mundo'
 // }, function(resp) {
 //     console.log('respuesta server: ', resp);
@@ -35,22 +42,21 @@ socket.on('disconnect', function() {
 
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-
     console.log('Servidor:', mensaje);
-
+    renderizarMensajes(mensaje, false);
+    scrollBottom();
 });
 
-// Escuchar cambios de Usuarios
+// Escuchar cambios de usuarios
 // cuando un usuario entra o sale del chat
-
 socket.on('listaPersona', function(personas) {
-
     console.log(personas);
-
+    renderizarUsuarios(personas);
 });
 
-//  mesnajes privados
-
+// Mensajes privados
 socket.on('mensajePrivado', function(mensaje) {
-    console.log('mensaje privado:', mensaje);
-})
+
+    console.log('Mensaje Privado:', mensaje);
+
+});
